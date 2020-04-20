101ca11c7821e55d7ac4d84eae8100aa5d56e557
MatteoCrusca01 <crusca.matteo@iisgalvanimi.edu.it>
2020-04-20 14:43:37 +0000

    Fine progetto

--- Crusca-tombola/src/app/app.module.ts
+++ Crusca-tombola/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartellaComponent } from './cartella/cartella.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartellaComponent
   ],
   imports: [
     BrowserModule,
