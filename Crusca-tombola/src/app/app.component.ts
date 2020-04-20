f57e8fe328ba7f91215d7943e45edc67d2651a87
MatteoCrusca01 <crusca.matteo@iisgalvanimi.edu.it>
2020-04-20 14:17:23 +0000

    punto3

--- Crusca-tombola/src/app/app.component.ts
+++ Crusca-tombola/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Crusca-tombola';
+  numeriEstratti: Array<number>=[];
+  ultimoNumero:string="...";
+  estrazioneNumero(){
+    var verifica=false;
+    while(verifica==false){
+      var n= Math.round(Math.random()*99+1)
+      verifica=true;
+      for(var i = 0; i< this.numeriEstratti.length; i++){
+        if(n == this.numeriEstratti[i]){
+          verifica = false;
+        }
+      }
+      if(verifica){
+      this.numeriEstratti.push(n)
+      console.log(n);
+      var scritta=document.getElementById("shownumber");
+      scritta.innerHTML=String(n);
+      this.ultimoNumero = ""+n;
+      if(this.numeriEstratti.length==100){
+        console.log("sono usciti tutti i numeri");
+        var bottone = document.getElementById("button");
+        bottone.setAttribute("disabled","disabled");
+        alert("Tutti i numeri sono stati estratti");
+        return false;
+      }
+    }
+
+    }
+    return false;
+  }
 }
